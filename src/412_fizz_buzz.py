3a7ecff8b7ea8384be850b87f9f49e4ce0b689a1
rongc <procr@qq.com>
2017-02-02 01:36:07 +0800

    412 easy

---
+++ src/412_fizz_buzz.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return [(("Fizz" * (not i % 3) + "Buzz" * (not i % 5)) or str(i)) for i in xrange(1, n + 1)]
+
+sol = Solution()
+print sol.fizzBuzz(15)
