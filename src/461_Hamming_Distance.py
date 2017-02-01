ce8f1f6632b9bc1c543ee7e80f2b96919465d132
rongc <procr@qq.com>
2017-02-01 11:01:51 +0800

    461 bin easy

---
+++ src/461_Hamming_Distance.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x^y).count("1")
+
+
+sol = Solution()
+print sol.hammingDistance(1, 4)
